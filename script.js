39567363dd9a8f81d05c34cb537097506d71fd89
ealikan <ethem_alikan@hotmail.com>
2023-08-04 14:30:35 +0300

    kopyalama butonu ve seslendirme butonu eklendi

--- script.js
+++ script.js
@@ -102,6 +102,8 @@ const toLang = document.querySelector("#to-lang")
 const btnTranslate = document.querySelector("#btnTranslate")
 const fromText = document.querySelector("#from-text")
 const toText = document.querySelector("#to-text")
+const exchange = document.querySelector(".exchange")
+const icons = document.querySelectorAll(".icons")
 
 
 
@@ -129,3 +131,35 @@ btnTranslate.addEventListener("click",()=>{
     })
 })
 
+exchange.addEventListener("click",()=>{
+    let text = fromText.value
+    fromText.value = toText.value
+    toText.value = text
+
+    let lang = fromLang.value
+    fromLang.value=toLang.value
+    toLang.value=lang
+})
+
+for(let icon of icons){
+    icon.addEventListener("click",(element)=>{
+        if(element.target.classList.contains("fa-copy")){
+            if(element.target.id == "from"){
+                navigator.clipboard.writeText(fromText.value)
+
+            }else{
+                navigator.clipboard.writeText(toText.value)
+            }
+        }else{
+            let utterance
+            if(element.target.id == "from"){
+                utterance = new SpeechSynthesisUtterance(fromText.value)
+                utterance.lang = fromLang.value
+            }else{
+                utterance = new SpeechSynthesisUtterance(toText.value)
+                utterance.lang = toLang.value
+            }
+            speechSynthesis.speak(utterance)
+        }
+    })
+}
